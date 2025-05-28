e6a05138e8256c5cbb0643016faf8e61002b86e7
Thien1124 <npthien124@gmail.com>
2025-05-28 09:29:53 +0700

    Sửa đổi AddContactForm

--- ContactList/AddContactForm.cs
+++ ContactList/AddContactForm.cs
@@ -26,5 +26,20 @@ namespace ContactList
         {
 
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
